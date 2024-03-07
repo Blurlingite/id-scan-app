1bd97b04f8da2ff3f2c5cade6c6c71a8fb6fc051
Vishnu Dawah <vkdawah@gmail.com>
2024-03-07 16:39:00 -0500

    Can now open this project in a web browser

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Our ID Scanner App</Text>
+      <Text>Our ID Scanner App Group 6</Text>
       <StatusBar style="auto" />
     </View>
   );
